5f55285cd565fdda6b21ca6d2501ab7f0d6407f2
Rendy Setiawan <lierendysetiawan3232@gmail.com>
2026-01-15 11:18:11 +0800

    Change back to normal export, updated .env in production

--- server/db/prisma.ts
+++ server/db/prisma.ts
@@ -1,15 +1,6 @@
+import "dotenv/config";
 import { PrismaClient } from '../../prisma/generated/client'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+const prisma = new PrismaClient()
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined
-}
-
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export { prisma }
